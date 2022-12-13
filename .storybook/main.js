2f474f6589721570e998f5246af54c5baf146d32
Valentin Palkovic <valentin@chromatic.com>
2022-12-13 10:30:51 +0100

    Convert CSF 2 to CSF 3

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,30 +1,18 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "storybook-addon-rtl",
-    "storybook-addon-pseudo-states",
-    "@storybook/addon-a11y",
-  ],
+  "stories": ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "storybook-addon-rtl", "storybook-addon-pseudo-states", "@storybook/addon-a11y"],
   "framework": {
     name: "@storybook/angular",
-    options: {},
+    options: {}
   },
-  "staticDirs": [
-    {
-      from: "../stories/assets",
-      to: "/static/assets",
-    },
-  ],
+  "staticDirs": [{
+    from: "../stories/assets",
+    to: "/static/assets"
+  }],
   "features": {
     "interactionsDebugger": true
   },
-  "core": {},
+  "core": {}
   // "docs": {
   //   // You can change this value to `false` to remove all automaitcally generated Docs Pages
   //   // We recommend instead to use them, and remove the "Overview" pages.
@@ -32,4 +20,8 @@ module.exports = {
   //   // For more information take a look here: https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#docs-page
   //   "docsPage": "automatic",
   // },
+  ,
+  docs: {
+    docsPage: "automatic"
+  }
 };
